8c75000fec83593fdd68466191b10baa6829ae01
Sahil Rathore <sahilrathore605@gmail.com>
2026-05-01 20:42:49 +0530

    Implement Protected route component, enhance auth context, and update useAuth hook for user state management

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router"
 import Login from "./features/auth/pages/login.jsx"
 import Register from "./features/auth/pages/register.jsx"
+import Protected from "./features/auth/components/Protected"
 
 
 
@@ -16,6 +17,6 @@ export const router = createBrowserRouter([
     },
     {
         path: "/",
-        element: <h1>Home Page</h1>
+        element: <Protected><h1>Home Page</h1></Protected>
     }
 ])
